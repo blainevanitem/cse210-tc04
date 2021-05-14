18bd96869a9f3cf7aaccd9e0029a6d0a0ccf1219
Steven Briskey <stevenjb96@gmail.com>
2021-05-14 14:27:26 -0600

    skeleton

---
+++ hilo/game/Dealer.py
@@ -0,0 +1,24 @@
+class Dealer:
+    def __init__(self):
+        self.cards = []
+        self.guess = ""
+
+
+        pass
+    def get_points(self):
+        '''
+        If True +100 points
+        If False - 75 points
+        return points
+        '''
+    def deal_cards(self):
+        '''
+        get 2 cards 
+        '''
+        pass
+    def get_guess(self):
+        '''
+        display card
+        self.guess = input
+        '''
+        pass
---
+++ hilo/game/Director.py
@@ -0,0 +1,30 @@
+class Director:
+    def __init__(self,):
+        self.keep_playing = True
+        self.score = 300
+        self.dealer = game.Dealer()
+    def start_game(self):
+        while self.keep_playing == True:
+            self.get_inputs()
+            self.do_points()
+            self.display_outputs()
+    def get_inputs(self):
+        self.dealer.deal_card()
+        self.dealer.get_guess()
+    def do_points(self):
+        points = self.dealer.get_points()
+        self.score += points
+    def display_outputs(self):
+
+        pass
+    def can_play(self):
+        '''
+        If score bad   
+            keep_playing = False
+        else
+            whant to play = input
+            if yes
+                self.keep playing = True
+            else
+                self.keep playing = False
+        '''    
